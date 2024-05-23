0792d82d1e898721da7ce90e1584161bb82eeb69
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-05-23 15:45:20 +0700

    git

---
+++ TeamProject/src/main/java/com/mycompany/teamproject/TeamProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teamproject;
+
+/**
+ *
+ * @author Dell
+ */
+public class TeamProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
