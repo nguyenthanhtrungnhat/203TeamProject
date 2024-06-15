4a1ece5a7271bc95e6d171d9689eb90f563e5bf7
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-06-15 10:47:53 +0700

    giit

---
+++ IELTS_PROJECT/src/main/java/com/mycompany/ielts_project/IELTS_PROJECT.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ielts_project;
+
+/**
+ *
+ * @author Dell
+ */
+public class IELTS_PROJECT {
+
+    public static void main(String[] args) {
+MainFrame mainFrame=new MainFrame();
+mainFrame.setVisible(true);
+    }
+}
